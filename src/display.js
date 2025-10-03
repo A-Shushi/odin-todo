9dce2f2c9e805ef67c0577c2bc474b33ccbe44fb
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-10-03 14:11:25 +0700

    Create function to display task list in HTML

--- src/display.js
+++ src/display.js
@@ -1,9 +1,11 @@
 const sidebarProjects = document.querySelector("#project-list")
+const projectContainer = document.querySelector("#project-container")
 
 function renderSidebar(projectArray) {
     while (sidebarProjects.firstChild) {
         sidebarProjects.removeChild(sidebarProjects.firstChild)
     }
+
     const newListItem = document.createElement("li");
     newListItem.id = "inbox-list-item"
 
@@ -17,6 +19,7 @@ function renderSidebar(projectArray) {
     newListItem.appendChild(projectButton)
 
     sidebarProjects.appendChild(newListItem)
+
     for (let i = 1; i < projectArray.length; i++) {
         const newListItem = document.createElement("li");
         newListItem.className = "project-list-item"
@@ -35,7 +38,7 @@ function renderSidebar(projectArray) {
         deleteButton.textContent = "X"
         deleteButton.addEventListener("click", (event) => {
             console.log(event)
-            console.log("DELETE")
+            console.log(`DELETE Project ${i}`)
         })
         newListItem.appendChild(deleteButton)
 
@@ -43,4 +46,57 @@ function renderSidebar(projectArray) {
     }
 }
 
-export {renderSidebar}
+function renderProject(project) {
+    while (projectContainer.firstChild) {
+        projectContainer.removeChild(projectContainer.firstChild)
+    }
+
+    const newProjectHeader = document.createElement("h1")
+    newProjectHeader.textContent = project.name;
+    projectContainer.appendChild(newProjectHeader)
+
+    const newUnorderedList = document.createElement("ul")
+    newUnorderedList.id = "todo-list";
+    projectContainer.appendChild(newUnorderedList);
+
+    for (let i = 0; i < project.todoArray.length; i++) {
+        const newListItem = document.createElement("li");
+        newListItem.className = "todo-list-item"
+
+        const newCheckbox = document.createElement("input")
+        newCheckbox.type = "checkbox";
+        newCheckbox.name = "task-check";
+        newCheckbox.id = `${i}`;
+        newCheckbox.addEventListener('change', function () {
+            if (this.checked) {
+                console.log(`${project.todoArray[i].title} is checked`);
+            } else {
+                console.log(`${project.todoArray[i].title} is unchecked`);
+            }
+        });
+        newListItem.appendChild(newCheckbox);
+
+        const newDueDate = document.createElement("p");
+        newDueDate.className = "due-date";
+        newDueDate.textContent = project.todoArray[i].dueDate
+        newListItem.appendChild(newDueDate);
+
+        const newTaskTitle = document.createElement("p");
+        newTaskTitle.className = "todo-item";
+        newTaskTitle.textContent = project.todoArray[i].title;
+        newListItem.appendChild(newTaskTitle)
+
+        const deleteButton = document.createElement("button")
+        deleteButton.className = "delete";
+        deleteButton.textContent = "X"
+        deleteButton.addEventListener("click", (event) => {
+            console.log(event)
+            console.log(`DELETE TASK ${i}`)
+        })
+        newListItem.appendChild(deleteButton)
+
+        newUnorderedList.appendChild(newListItem)
+    }
+}
+
+export {renderSidebar, renderProject}
--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ projectStorage.createProject("New Project")
 projectStorage.createProject("Second Project")
 
 // Create new note and append to storage
-createTodo(0, "Index Title", "Description", "DueDate", "Priority")
-createTodo(1, "First Title", "Description", "DueDate", "Priority")
-createTodo(2, "First Title", "Description", "DueDate", "Priority")
+createTodo(0, "Index Title", "Description", "20 Sept", "Priority")
+createTodo(1, "First Title", "Description", "21 Sept", "Priority")
+createTodo(2, "First Title", "Description", "22 Sept", "Priority")
 
 console.log(projectStorage.projectArray)
 console.log(projectStorage.projectArray[0].todoArray)
@@ -20,3 +20,4 @@ console.log(projectStorage.projectArray[1].todoArray)
 console.log(projectStorage.projectArray[2].todoArray)
 
 display.renderSidebar(projectStorage.projectArray)
+display.renderProject(projectStorage.projectArray[0])
